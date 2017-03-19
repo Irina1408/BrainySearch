6a1ee612449b43ec4bd58b6dc86e8e4bd91d8f41
Irina1408 <irina.emtsova@gmail.com>
2017-03-19 13:26:29 +0200

    Add project files.

---
+++ InfoFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InfoFinder.MvcApplication" Language="C#" %>
---
+++ InfoFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfoFinder.Startup))]
+namespace InfoFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
